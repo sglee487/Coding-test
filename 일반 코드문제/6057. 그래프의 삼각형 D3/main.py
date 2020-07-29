c1bf208b0ff997c8dfe79f1bf829322e584b370f
gun <sglee487@gmail.com>
2020-07-29 16:07:42 +0900

    20.07.28 커밋. 6057 문제 품.

--- 일반 코드문제/6057. 그래프의 삼각형 D3/main.py
+++ 일반 코드문제/6057. 그래프의 삼각형 D3/main.py
@@ -1,24 +1,23 @@
 import sys
-from collections import deque
 
 sys.stdin = open("s_input.txt", "r")
 
-def go():
+def dfs(si,j,rank):
     global tri_count
-
-    Q = deque()
-    for n in range(1,N+1):
-        Q.append((n,ways[n],1))
-    while Q:
-        si, way, rank = Q.pop()
-        for w in range(1, N+1):
-
-
+    if rank == 3:
+        if si == j: tri_count += 1
+        return
+    for i in range(1, N+1):
+        if ways[j][i] == 1 and not visited[i]:
+            visited[i] = True
+            dfs(si, i, rank+1)
+            visited[i] = False
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
+    # https://dheldh77.tistory.com/entry/SWEA-6057-%EA%B7%B8%EB%9E%98%ED%94%84%EC%9D%98-%EC%82%BC%EA%B0%81%ED%98%95
     N, M = map(int, input().split())
     ways = [[0] * (N + 1) for _ in range(N + 1)]
     for _ in range(M):
@@ -26,10 +25,16 @@ for test_case in range(1, T + 1):
         ways[a][b], ways[b][a] = 1, 1
 
     tri_count = 0
-    print(ways)
+    visited = [False] * (N + 1)
 
-    go()
+    for i in range(1, N+1):
+        for j in range(1, N+1):
+            if j < i: visited[j] = True
+            else: visited[j] = False
+        for j in range(1, N+1):
+            if not visited[j] and ways[i][j] == 1:
+                visited[j] = True
+                dfs(i,j,1)
 
-
-    print("#{}".format(test_case))
+    print("#{}".format(test_case),tri_count)
     # ///////////////////////////////////////////////////////////////////////////////////
