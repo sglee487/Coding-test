dc987336714a0864f5f2c590bb022ae407bfb601
gun <sglee487@gmail.com>
2020-08-02 22:38:56 +0900

    20.08.02 커밋. 4579 문제 품.

---
+++ 일반 코드문제/4579. 세상의 모든 팰린드롬 2 D3/main.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.stdin = open("sample_input2.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+    st = input()
+    # print(st)
+
+    pen = True
+    for i in range(len(st)//2):
+        # print(i, -(i + 1), st[i], st[-(i + 1)])
+        if st[i] != st[-(i+1)] and st[i] != '*' and st[-(i+1)] != '*':
+            if '*' not in st[-(i+1):] and '*' not in st[:i]:
+                pen = False
+                break
+
+    # print(pen)
+    if pen: ex = 'Exist'
+    else: ex = 'Not exist'
+
+    print("#{}".format(test_case),ex)
+    # ///////////////////////////////////////////////////////////////////////////////////
