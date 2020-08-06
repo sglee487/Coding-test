c787832466179c443980fdc63404be0610529cff
gun <sglee487@gmail.com>
2020-08-07 02:30:03 +0900

    20.08.07 커밋. 5432 문제 품.

--- 일반 코드문제/5432. 쇠막대기 자르기 D4/main.py
+++ 일반 코드문제/5432. 쇠막대기 자르기 D4/main.py
@@ -1,7 +1,6 @@
-from collections import deque
 import sys
 
-sys.stdin = open("sample_input.txt", "r")
+sys.stdin = open("sample_input2.txt", "r")
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -9,8 +8,19 @@ for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
     pars = input()
 
-    ps = deque()
+    bars = 0
+    stack_count = 0
 
+    for i in range(len(pars)):
+        if pars[i] == '(':
+            stack_count += 1
+        if pars[i] == ')':
+            if pars[i-1] == '(':
+                stack_count -= 1
+                bars += stack_count
+            else:
+                stack_count -= 1
+                bars += 1
 
-    print("#{}".format(test_case))
+    print("#{}".format(test_case),bars)
     # ///////////////////////////////////////////////////////////////////////////////////
