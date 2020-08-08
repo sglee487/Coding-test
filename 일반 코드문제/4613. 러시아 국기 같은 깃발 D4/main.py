ff533fcd2bf378dc470bcbc7d035829845a92b7b
gun <sglee487@gmail.com>
2020-08-08 18:37:57 +0900

    20.08.08 커밋. 4613 문제 푸는중

--- 일반 코드문제/4613. 러시아 국기 같은 깃발 D4/main.py
+++ 일반 코드문제/4613. 러시아 국기 같은 깃발 D4/main.py
@@ -17,11 +17,31 @@ for test_case in range(1, T + 1):
     RB = [[1 if c == 'R' else 0 for c in s] for s in board]
 
     wsl, bsl, rsl = 0, 0, 0
+
+    other_blocks = 0
+
     max_avg = 0
-    print(sum(WB[0]))
     # white
-    # for i in range(wsl,N-2):
-    #     avg =
+    for i in range(wsl+1,N-1):
+        avg = sum([sum(col) for col in zip(*WB[wsl:i])]) / ((i - wsl) * M)
+        if avg > max_avg:
+            max_avg = avg
+            bsl = i
+
+    max_avg = 0
+    # blue
+    for i in range(bsl + 1, N):
+        avg = sum([sum(col) for col in zip(*BB[bsl:i])]) / ((i - bsl) * M)
+        if avg > max_avg:
+            max_avg = avg
+            rsl = i
+
+    all_sum = sum([sum(col) for col in zip(*WB[wsl:bsl])]) \
+              + sum([sum(col) for col in zip(*BB[bsl:rsl])]) \
+              + sum([sum(col) for col in zip(*RB[rsl:N])])
+
+    print(all_sum)
+    print(N*M - all_sum)
 
 
     print("#{}".format(test_case))
