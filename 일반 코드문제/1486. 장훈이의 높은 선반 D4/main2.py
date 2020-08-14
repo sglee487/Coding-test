416580482f42238ee233c8bc730c45da610ecf43
gun <sglee487@gmail.com>
2020-08-14 17:05:17 +0900

    20.08.14 커밋. 1486 문제 다른 방법으로 풀고 시간 비교.

--- 일반 코드문제/1486. 장훈이의 높은 선반 D4/main2.py
+++ 일반 코드문제/1486. 장훈이의 높은 선반 D4/main2.py
@@ -2,16 +2,22 @@ import sys
 
 sys.stdin = open("input.txt", "r")
 
-def allPartSum(i,K,S,R):
-    global lowest_H, B
-    if i == -1:
-        if S >= K and lowest_H > S:
-            lowest_H = S
-        return
-    if S > lowest_H: return
-    if R < K: return
-    allPartSum(i - 1, K, S + H[i], R)
-    allPartSum(i - 1, K, S,R - H[i])
+# 173 ms
+def solve(N,B,allsum):
+    lowest_H = 200000
+    stack = []
+
+    stack.append((N-1,0,allsum))
+    while stack:
+        i, S, R = stack.pop()
+        if S >= B:
+            lowest_H = min(lowest_H,S)
+        if R < B: continue
+        if i >= 0:
+            stack.append((i - 1, S + H[i], R))
+            stack.append((i - 1, S, R - H[i]))
+
+    return lowest_H
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -20,13 +26,10 @@ for test_case in range(1, T + 1):
     N, B = map(int, input().split())
     H = list(map(int, input().split()))
 
-    lowest_H = 200000
-
     allsum = sum(H)
 
-    # 213 ms
-    allPartSum(N-1,B,0,allsum)
-
+    # 173 ms
+    lowest_H = solve(N,B,allsum)
 
     print("#{}".format(test_case), lowest_H - B)
     # ///////////////////////////////////////////////////////////////////////////////////
