3e5343b0b612de51f50aa81120795449b9a05183
gun <sglee487@gmail.com>
2020-08-17 00:27:45 +0900

    20.08.16 커밋. 1873 문제 품.

--- 일반 코드문제/1873. 상호의 배틀필드 D3/main.py
+++ 일반 코드문제/1873. 상호의 배틀필드 D3/main.py
@@ -2,9 +2,6 @@ import sys
 
 sys.stdin = open("input2.txt", "r")
 
-def shoot(p,d):
-    return
-
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
@@ -16,28 +13,113 @@ for test_case in range(1, T + 1):
     _ = input()
     inpl = list(input())
 
-    print(*Map,sep='\n')
-    print(inpl)
+    # print(*Map,sep='\n')
+    # print(inpl)
 
-    tank = (-1,-1)
+    tank_y, tank_x, tank_d = -1, -1, ''
     for r in range(H):
         for c in range(W):
             if Map[r][c] == '<' or Map[r][c] == '>' or Map[r][c] == '^' or Map[r][c] == 'v':
-                tank = (r,c)
+                tank_y, tank_x, tank_d = r, c, Map[r][c]
                 break
-        if tank != (-1,-1): break
+        if tank_y != -1 and tank_x != -1: break
 
     for inp in inpl:
         if inp == 'S':
-            pass
+            if tank_d == '<':
+                po_x = tank_x
+                po_x -= 1
+                while po_x >= 0:
+                    # if Map[tank_y][po_x] == '.':
+                    #     continue
+                    if Map[tank_y][po_x] == '*':
+                        Map[tank_y][po_x] = '.'
+                        break
+                    if Map[tank_y][po_x] == '#':
+                        break
+                    po_x -= 1
+            if tank_d == '>':
+                po_x = tank_x
+                po_x += 1
+                while po_x < W:
+                    # if Map[tank_y][po_x] == '.':
+                    #     continue
+                    if Map[tank_y][po_x] == '*':
+                        Map[tank_y][po_x] = '.'
+                        break
+                    if Map[tank_y][po_x] == '#':
+                        break
+                    po_x += 1
+            if tank_d == '^':
+                po_y = tank_y
+                po_y -= 1
+                while po_y >= 0:
+                    # if Map[po_y][tank_x] == '.':
+                    #     continue
+                    if Map[po_y][tank_x] == '*':
+                        Map[po_y][tank_x] = '.'
+                        break
+                    if Map[po_y][tank_x] == '#':
+                        break
+                    po_y -= 1
+            if tank_d == 'v':
+                po_y = tank_y
+                po_y += 1
+                while po_y < H:
+                    # if Map[po_y][tank_x] == '.':
+                    #     continue
+                    if Map[po_y][tank_x] == '*':
+                        Map[po_y][tank_x] = '.'
+                        break
+                    if Map[po_y][tank_x] == '#':
+                        break
+                    po_y += 1
         if inp == 'U':
-            pass
+            tank_d = '^'
+            if tank_y - 1 < 0:
+                Map[tank_y][tank_x] = '^'
+                continue
+            if Map[tank_y-1][tank_x] == '*' or Map[tank_y-1][tank_x] == '#' or Map[tank_y-1][tank_x] == '-':
+                Map[tank_y][tank_x] = '^'
+                continue
+            Map[tank_y - 1][tank_x], Map[tank_y][tank_x] = '^', '.'
+            tank_y = tank_y - 1
         if inp == 'D':
-            pass
+            tank_d = 'v'
+            if tank_y + 1 >= H:
+                Map[tank_y][tank_x] = 'v'
+                continue
+            if Map[tank_y+1][tank_x] == '*' or Map[tank_y+1][tank_x] == '#' or Map[tank_y+1][tank_x] == '-':
+                Map[tank_y][tank_x] = 'v'
+                continue
+            Map[tank_y + 1][tank_x], Map[tank_y][tank_x] = 'v', '.'
+            tank_y = tank_y + 1
         if inp == 'L':
-            pass
+            tank_d = '<'
+            if tank_x - 1 < 0:
+                Map[tank_y][tank_x] = '<'
+                continue
+            if Map[tank_y][tank_x-1] == '*' or Map[tank_y][tank_x-1] == '#' or Map[tank_y][tank_x-1] == '-':
+                Map[tank_y][tank_x] = '<'
+                continue
+            Map[tank_y][tank_x-1], Map[tank_y][tank_x] = '<', '.'
+            tank_x = tank_x - 1
         if inp == 'R':
-            pass
+            tank_d = '>'
+            if tank_x + 1 >= W:
+                Map[tank_y][tank_x] = '>'
+                continue
+            if Map[tank_y][tank_x+1] == '*' or Map[tank_y][tank_x+1] == '#' or Map[tank_y][tank_x+1] == '-':
+                Map[tank_y][tank_x] = '>'
+                continue
+            Map[tank_y][tank_x+1], Map[tank_y][tank_x] = '>', '.'
+            tank_x = tank_x + 1
+        # print(inp)
+        # print(*Map, sep='\n')
 
-    print("#{}".format(test_case))
+
+    print("#{}".format(test_case),end=' ')
+    print(*Map[0],sep='')
+    for i in range(1,H):
+        print(*Map[i], sep='')
     # ///////////////////////////////////////////////////////////////////////////////////
