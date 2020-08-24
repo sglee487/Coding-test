923b78fc1027dc9a7224c37c77b787a060691b07
gun <sglee487@gmail.com>
2020-08-24 21:57:32 +0900

    20.08.24 커밋. 5987 문제 품.

--- 일반 코드문제/5987. 달리기 D4/main.py
+++ 일반 코드문제/5987. 달리기 D4/main.py
@@ -2,11 +2,29 @@ import sys
 
 sys.stdin = open("s_input.txt", "r")
 
+def dfs(now):
+    if now == end: return 1
+    if cases[now] : return cases[now]
+    for i in range(N):
+        if now & (1 << i) == 0 and (now & left[i]) == left[i]:
+            cases[now] += dfs(now | (1 << i))
+    return cases[now]
+
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
     N, M = map(int, input().split())
 
-    print("#{0}".format(test_case))
+    left = [0] * N
+    cases = [0] * (1 << N)
+    for _ in range(M):
+        a, b = map(int, input().split())
+        left[b-1] |= 1 << (a-1)
+
+    end = (1<<N) - 1
+
+    result = dfs(0)
+
+    print("#{0}".format(test_case), result)
     # ///////////////////////////////////////////////////////////////////////////////////
