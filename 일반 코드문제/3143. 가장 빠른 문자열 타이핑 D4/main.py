9ac43fd4096c5b4ee4aa6325ffa5e505379dc0a0
gun <sglee487@gmail.com>
2020-08-28 01:04:25 +0900

    20.08.27 커밋. 3143 문제 품(보이어-무어 알고리즘).

--- 일반 코드문제/3143. 가장 빠른 문자열 타이핑 D4/main.py
+++ 일반 코드문제/3143. 가장 빠른 문자열 타이핑 D4/main.py
@@ -10,29 +10,27 @@ for test_case in range(1, T + 1):
 
     si = 0
     match_num = 0
-    i=0
-    j=0
-    while i < len(A):
-        print(i)
-        while A[i] == B[j] and j < len(B):
-            i += 1
-            j += 1
-            print(i,j)
-            if i == len(A) or j == len(B): break
-        if i == len(A) or j == len(B): break
-        if j == len(B):
+    i = 0
+    while i <= len(A) - len(B):
+        j = len(B) - 1
+        while j >= 0:
+            if A[i + j] != B[j]:
+                k = len(B)-2
+                while k >= 0:
+                    if A[i + j] == B[k]: break
+                    k -= 1
+                if k != -1:
+                    i += len(B) - k - 1
+                else:
+                    i += len(B)
+                break
+            j -= 1
+
+        if j == -1:
             match_num += 1
-        else:
-            while A[i] != B[j]:
-                j -= 1
-                if j == -1: break
-            i -= j
-        i += 1
-        j = 0
-    print(match_num)
+            i += len(B)
 
+    result = len(A) - (len(B) * match_num) + match_num
 
-
-
-    print("#{}".format(test_case))
+    print("#{}".format(test_case), result)
     # ///////////////////////////////////////////////////////////////////////////////////
