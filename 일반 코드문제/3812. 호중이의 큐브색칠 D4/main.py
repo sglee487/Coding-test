fe87197d2907988f50f95a245ad74842060a8d0b
gun <sglee487@gmail.com>
2020-08-29 16:25:38 +0900

    20.08.29 커밋. 3812 문제 푸는중.

--- 일반 코드문제/3812. 호중이의 큐브색칠 D4/main.py
+++ 일반 코드문제/3812. 호중이의 큐브색칠 D4/main.py
@@ -7,17 +7,30 @@ T = int(input())
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
     X, Y, Z, A, B, C, N = map(int, input().split())
+    print(X, Y, Z, A, B, C, N)
 
     colors = [0] * N
     garo = [0] * N
 
-    x_end, y_end, z_end = (X // N) * N, (Y // N) * N, (Z // N) * N
+    x_end, y_end, z_end = ((X-A) // N) * N + A, ((Y-B) // N) * N + B, ((Z-C) // N) * N + C
+    x_zero, y_zero, z_zero = A-((A // N) * N), B-((B // N) * N), C-((C // N) * N)
     print(x_end,y_end,z_end)
-    not_remain_x = (X // N) * Y * Z
-    not_remain_y = (X-x_end) * (Y // N) * Z
-    not_remain_z = (X-x_end) * (Y-y_end) * (Z // N)
-    print(not_remain_x,not_remain_y,not_remain_z)
-    colors = [not_remain_x + not_remain_y + not_remain_z] * N
+    print(x_zero,y_zero,z_zero)
+    axis = int()
+    gongtong = 0
+    if x_end - x_zero == 0 and x_end - x_zero == 0 and x_end - x_zero == 0:
+        axis = -1
+    elif x_end - x_zero != 0:
+        gongtong = (((x_end - x_zero) * (Y) * (Z)) // N)
+        axis = 0
+    elif y_end - y_zero != 0:
+        gongtong = (((X) * (y_end - y_zero) * (Z)) // N)
+        axis = 1
+    elif z_end - z_zero != 0:
+        gongtong = (((X) * (Y) * (z_end - z_zero)) // N)
+        axis = 2
+
+    colors = [gongtong] * N
 
 
     print(colors)
