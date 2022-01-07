b365c71185eae7e384170d848c478816f3f97a02
sglee <sglee487@gmail.com>
2022-01-07 23:48:52 +0900

    22.01.07

--- Backjoon/2229-조 짜기.py
+++ Backjoon/2229-조 짜기.py
@@ -10,39 +10,16 @@ input = sys.stdin.readline
 N = int(input())
 nl = list(map(int, input().split()))
 
-print(N)
-print(nl)
-max_values = [[0] * N for _ in range(N)]
-min_values = [[0] * N for _ in range(N)]
-scores = [[0] * N for _ in range(N)]
-for i in range(N):
+# print(N)
+# print(nl)
+
+dp = [0] * (N+1)
+for i in range(1, N):
+    min_value = 10001
     max_value = 0
-    min_value = 1001
-    for j in range(i,N):
-        max_value = max(nl[j], max_value)
-        min_value = min(nl[j], min_value)
-        scores[i][j] = max_value - min_value
-        max_values[i][j] = max_value
-        min_values[i][j] = min_value
+    for j in range(i,-1,-1):
+        min_value = min(min_value, nl[j])
+        max_value = max(max_value, nl[j])
+        dp[i+1] = max(dp[i+1], max_value - min_value + dp[j-1+1])
 
-print('-'*10)
-print(*scores, sep='\n')
-
-dp = [-1] * (N+1)
-def dfs(start, now):
-    global N, scores
-    if dp[start] != -1:
-        return dp[start]
-    dp[start] = 0
-    value = 0
-    print(start, N, now)
-    if start == N:
-        value = max(now, value)
-    for l in range(N-start):
-        value = max(value, dfs(start+l+1, now + scores[start][start+l]))
-    dp[start] = value
-    return value
-
-dfs(0,0)
-
-print(dp)
+print(dp[N])
