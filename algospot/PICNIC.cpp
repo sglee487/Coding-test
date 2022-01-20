ff6c1aa33c1164b13fcc134f0b45c82f73c91b9c
sglee <sglee487@gmail.com>
2022-01-21 00:01:26 +0900

    종만북 6-3

--- algospot/PICNIC.cpp
+++ algospot/PICNIC.cpp
@@ -5,11 +5,31 @@
 
 int C, n, m;
 int friends[11][11];
+int picked[11];
 
-int dfs(int step,int N,int friends[11][11]){
-    for (int i=step; i<N; i++){
-
+int dfs(int N,int picked[11], int friends[11][11]){
+    int first = -1;
+    for (int i=0;i<N;i++){
+        if (!picked[i]){
+            first = i;
+            break;
+        }
     }
+    // std::cout << first << std::endl;
+    if (first == -1){
+        return 1;
+    }
+    int result = 0;
+    for (int pair = first+1; pair < N; pair++){
+        if (!picked[pair] && friends[first][pair] == 1){
+            picked[first] = 1, picked[pair] = 1;
+            // std::cout << first << pair << std::endl;
+            result += dfs(N, picked, friends);
+            picked[first] = 0, picked[pair] = 0;
+        }
+    }
+
+    return result;
 }
 
 int main(){
@@ -22,6 +42,7 @@ int main(){
         ifs >> n >> m;
 
         for (int i=0;i<n;i++){
+            picked[i] = 0;
             for (int j=0;j<n;j++){
                 friends[i][j] = 0;
             }
@@ -34,7 +55,7 @@ int main(){
             friends[b][a] = 1;
         }
 
-        std::cout << dfs(0,n,friends) << std::endl;
+        std::cout << dfs(n,picked,friends) << std::endl;
 
 
     }
